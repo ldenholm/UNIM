c078c9f13a1d0882c22d81ab4e5a82f95da60bf6
Loki <disrute@gmail.com>
2021-03-22 21:20:20 +1100

    Add MassTransit

--- UNIM/Startup.cs
+++ UNIM/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using MassTransit;
 namespace UNIM
 {
     public class Startup
@@ -25,6 +18,10 @@ namespace UNIM
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(cfg =>
+            {
+                
+            });
             services.AddControllers();
         }
 
